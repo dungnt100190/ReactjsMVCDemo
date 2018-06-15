1ab2698a2a7885b2aa03a19cb69ddb8c503ed636
dungnt100190 <dungnt100190@gmail.com>
2018-06-15 15:13:30 +0700

    Create new solution & project from template

---
+++ DataModel/ContactDBContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLibrary.Models
+{
+    public partial class ContactDBContext : DbContext
+    {
+        public virtual DbSet<Contact> Contact { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+               // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer(@"Server=CMC-DUNGNT31;Database=ContactDB;User Id=CMC-DungNT31;Password=;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Contact>(entity =>
+            {
+                entity.HasKey(e => e.ContactId);
+
+                entity.Property(e => e.Email)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.FirstName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.LastName)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Phone)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+            });
+        }
+    }
+}
---
+++ DataModel/EntityRepository.cs
@@ -0,0 +1,117 @@
+﻿using DataAccessLibrary.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace DataModel.Services
+{
+    public interface IEntityRepository<T> : IDisposable where T : class
+    {
+        IQueryable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = "");
+        T Get(object key);
+        T Get(object[] keys);
+        bool Insert(T entity);
+        bool Update(T entity);
+        bool Delete(object key);
+        bool Delete(T entity);
+    }
+
+    public class EntityRepository<T> : IEntityRepository<T> where T : class
+    {
+        private ContactDBContext _context;
+        private DbSet<T> _dbSet;
+
+        public EntityRepository(ContactDBContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<T>();
+        }
+
+        public IQueryable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = "")
+        {
+            IQueryable<T> query = _dbSet;
+
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+
+            foreach (var includeProperty in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+            {
+                query = query.Include(includeProperty);
+            }
+
+            if (orderBy != null)
+            {
+                query = orderBy(query);
+            }
+
+            return query;
+        }
+
+        public T Get(object key)
+        {
+            return _dbSet.Find(key);
+        }
+
+        public T Get(object[] keys)
+        {
+            return _dbSet.Find(keys);
+        }
+
+        public bool Insert(T entity)
+        {
+            _dbSet.Add(entity);
+            return _context.SaveChanges() >= 1;
+        }
+
+        public bool Update(T entity)
+        {
+            _dbSet.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+            return _context.SaveChanges() >= 1;
+        }
+
+        public bool Delete(object key)
+        {
+            T entity = _dbSet.Find(key);
+            return Delete(entity);
+        }
+
+        public bool Delete(T entity)
+        {
+            if (_context.Entry(entity).State == EntityState.Detached)
+            {
+                _dbSet.Attach(entity);
+            }
+            _dbSet.Remove(entity);
+            return _context.SaveChanges() >= 1;
+        }
+
+        #region IDisposable Support
+        private bool disposed = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    _context.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
