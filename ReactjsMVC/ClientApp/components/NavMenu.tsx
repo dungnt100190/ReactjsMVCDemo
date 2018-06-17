4cf03e74c4bd848080d2b12319335be454ecd359
dungnt100190 <dungnt100190@gmail.com>
2018-06-17 17:18:34 +0700

    Complete Demo

--- ReactjsMVC/ClientApp/components/NavMenu.tsx
+++ ReactjsMVC/ClientApp/components/NavMenu.tsx
@@ -12,29 +12,14 @@ export class NavMenu extends React.Component<{}, {}> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>ReactJSDemo</Link>
+                    <Link className='navbar-brand' to={ '/contact' }>ReactJSDemo</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
                     <ul className='nav navbar-nav'>
                         <li>
-                            <NavLink to={ '/' } exact activeClassName='active'>
-                                <span className='glyphicon glyphicon-home'></span> Home
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/counter' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-education'></span> Counter
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={ '/fetchdata' } activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Fetch data
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to={'/contact'} activeClassName='active'>
-                                <span className='glyphicon glyphicon-th-list'></span> Contact data
+                            <NavLink to={ '/contact' } exact activeClassName='active'>
+                                <span className='glyphicon glyphicon-home'></span> Contact data
                             </NavLink>
                         </li>
                     </ul>
--- ReactjsMVC/ClientApp/routes.tsx
+++ ReactjsMVC/ClientApp/routes.tsx
@@ -1,14 +1,9 @@
 import * as React from 'react';
 import { Route } from 'react-router-dom';
 import { Layout } from './components/Layout';
-import { Home } from './components/Home';
-import { FetchData } from './components/FetchData';
-import { Counter } from './components/Counter';
 import { Contact } from './components/Contact/Contact';
 
 export const routes = <Layout>
-    <Route exact path='/' component={ Home } />
-    <Route path='/counter' component={ Counter } />
-    <Route path='/fetchdata' component={ FetchData } />
+    <Route exact path='/' component={ Contact } />
     <Route path='/contact' component={ Contact } />
 </Layout>;
